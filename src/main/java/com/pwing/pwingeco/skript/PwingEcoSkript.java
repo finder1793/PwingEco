20e2982e9d3cacf09293f8842c7326c020b5f352
finder1793 <kangas.kyle@gmail.com>
2024-12-22 02:07:08 +0000

    Rebase script cont

--- src/main/java/com/pwing/pwingeco/skript/PwingEcoSkript.java
+++ src/main/java/com/pwing/pwingeco/skript/PwingEcoSkript.java
@@ -1,14 +1,12 @@
 package com.pwing.pwingeco.skript;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.registrations.EventValues;
-import ch.njol.skript.util.Getter;
+import ch.njol.skript.lang.ExpressionType;
 import com.pwing.pwingeco.PwingEco;
 import com.pwing.pwingeco.skript.expressions.ExprBalance;
 import com.pwing.pwingeco.skript.effects.EffectGiveMoney;
 import com.pwing.pwingeco.skript.effects.EffectTakeMoney;
 import com.pwing.pwingeco.skript.conditions.ConditionHasMoney;
-import ch.njol.skript.lang.ExpressionType;
 
 public class PwingEcoSkript {
     private final PwingEco pwingEco;
@@ -26,14 +24,13 @@ public class PwingEcoSkript {
         EffectGiveMoney.setPwingEco(pwingEco);
         EffectTakeMoney.setPwingEco(pwingEco);
         ConditionHasMoney.setPwingEco(pwingEco);
-
-        Skript.registerCondition(ConditionHasMoney.class, "[pwingeco] %player% (has|have) [at least] %number% [of] [pwingeco] %string%");
+        
         Skript.registerEffect(EffectGiveMoney.class, "(give|add) %number% %string% to %player%");
         Skript.registerEffect(EffectTakeMoney.class, "(take|remove) %number% %string% from %player%");
+        Skript.registerCondition(ConditionHasMoney.class, "[pwingeco] %player% (has|have) [at least] %number% [of] [pwingeco] %string%");
         Skript.registerExpression(ExprBalance.class, Number.class, ExpressionType.PROPERTY);
     }
     
     private boolean isSkriptInstalled() {
         return pwingEco.getServer().getPluginManager().getPlugin("Skript") != null;
     }
-}
